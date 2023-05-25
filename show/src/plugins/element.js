baca73348b58c155cc2becce851af281007e0cae
wencai zhang <1772591173@qq.com>
2023-05-25 15:25:53 +0800

    Delete show directory

--- show/src/plugins/element.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import { Button, Input, Form, FormItem, Row, Col } from 'element-ui'
-
-Vue.use(Button)
-Vue.use(Input)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Row)
-Vue.use(Col)
